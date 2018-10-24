ade732b7c0aed85667449faf9a3d82b87a1369d5
zachblitz <zachblitz8@yahoo.com>
2018-10-24 10:03:13 -0400

    Add files via upload

---
+++ beginner-html-site-styled-gh-pages/scripts/main.js
@@ -0,0 +1,38 @@
+// var myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+
+// *************
+
+// var myImage = document.querySelector('img');
+
+// myImage.onclick = function() {
+//     var mySrc = myImage.getAttribute('src');
+//     if(mySrc === 'images/firefox-icon.png') {
+//       myImage.setAttribute ('src','images/becca.jpg');
+//     } else {
+//       myImage.setAttribute ('src','images/firefox-icon.png');
+//     }
+// }
+
+
+// *************
+// var myButton = document.querySelector('button');
+// var myHeading = document.querySelector('h1');
+
+// function setUserName() {
+//   var myName = prompt('Please enter your name.');
+//   localStorage.setItem('name', myName);
+//   myHeading.textContent = 'Mozilla is cool, ' + myName;
+// }
+
+
+// if(!localStorage.getItem('name')) {
+//   setUserName();
+// } else {
+//   var storedName = localStorage.getItem('name');
+//   myHeading.textContent = 'Mozilla is cool, ' + storedName;
+// }
+
+// myButton.onclick = function() {
+//   setUserName();
+// }
